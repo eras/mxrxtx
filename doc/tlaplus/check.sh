ff5083d4907232ae9b12b197393186eeb734bf4e
Erkki Seppälä <erkki.seppala@vincit.fi>
2023-02-05 14:56:16 +0200

    tlaplus: use workers in checking

--- doc/tlaplus/check.sh
+++ doc/tlaplus/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p trace
 ulimit -v 50000000
-tlc MC -difftrace -noGenerateSpecTEBin "$@" -teSpecOutDir trace -dump dot,colorize,actionlabels model
+tlc MC -workers auto -difftrace -noGenerateSpecTEBin "$@" -teSpecOutDir trace -dump dot,colorize,actionlabels model
 #tlc MC -noGenerateSpecTEBin "$@" -teSpecOutDir trace
 dot -Grankdir=LR -Tpdf model.dot > model.pdf
 skill -HUP llpp
