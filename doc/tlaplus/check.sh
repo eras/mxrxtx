d730e9a550930ee18ae0dfd65bfba9359234aa76
Erkki Seppälä <erkki.seppala@vincit.fi>
2023-02-13 18:37:35 +0200

    tlaplus/check.sh: don't process large dot files

--- doc/tlaplus/check.sh
+++ doc/tlaplus/check.sh
@@ -4,5 +4,10 @@ ulimit -v 50000000
 # for more recent TLC:
 #tlc MC -workers auto -noGenerateSpecTEBin "$@" -teSpecOutDir trace -dump dot,colorize,actionlabels model
 tlc MC -workers auto "$@" -teSpecOutDir trace -dump dot,colorize,actionlabels model
-dot -Grankdir=LR -Tpdf model.dot > model.pdf
-skill -HUP llpp
+# only dot models smaller than x bytes
+if du -b model.dot | awk '$1 > 1000000 { exit(1) }'; then
+    dot -Grankdir=LR -Tpdf model.dot > model.pdf
+    skill -HUP llpp
+else
+    echo "Skipped processing large dot file"
+fi
